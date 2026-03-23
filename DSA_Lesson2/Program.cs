8f9b22706f511ccbe4559458ae28024d6e651b6a
Thanh Nhan <2500115586@vanlangsaigon.edu.vn>
2026-03-23 08:51:40 +0700

    Hoan thanh thuc hanh Buoi 1 - Tham chieu va Swap

---
+++ DSA_Lesson2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static long GiaiThua(int n)
+    {
+        if (n <= 1) return 1;
+
+        return n * GiaiThua(n - 1);
+    }
+static int Fibonacci(int n){
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+    // buoc de quy : F(n) = F(n - 1) + F(n - 2)
+    return Fibonacci(n - 1) + Fibonacci (n - 2);
+}
+    static void Main(String[] args){
+        Console.Write("Nhap so n: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("{0}! = {1}",n,GiaiThua(n));
+        Console.WriteLine("Fibonacci({0}) = {1}",n,Fibonacci(n));
+    }
+}
