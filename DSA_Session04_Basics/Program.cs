e3c9d60c77486b6c4d6ed2e3307a2a3838f59d2f
Thanh Nhan <2500115586@vanlangsaigon.edu.vn>
2026-03-30 09:20:54 +0700

    Source code C# Buoi 05

--- DSA_Session04_Basics/Program.cs
+++ DSA_Session04_Basics/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+//Author: Lê Thành Nhân - 2500115586
+//Mục tiêu:
+//Bài 1:Lời chào thông minh
+//Yêu cầu: Nhập tên và MSSV,in ra lời chào.
+//Code:
+//Sử dụng Console.ReadLine() để gán biến.
+
+class Program {
+    static void Main() {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.Write("Nhap ho ten:");
+        String name = Console.ReadLine();
+        Console.Write("Nhap MSSV: ");
+        String mssv = Console.ReadLine();
+        Console.WriteLine($"Chao mung sinh vien {name} - (MS: {mssv}) den voi lop CTDL&GT!");
+
+        // bài 2: Máy tính cơ bản
+        //Yêu cầu: Nhập 2 số nguyên a và b. Tính toán và in ra tổng,hiệu,tích,thương.
+        //hướng dẫn: Dữ liệu từ bàn phím luôn là String,
+        //Các em phải dùng int.Parse() để chuyển về số nguyên trước khi tính toán.
+        Console.Write("Nhap so a:");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Nhap so b:");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Tong: {a + b}");
+        Console.WriteLine($"Hieu: {a - b}");
+        Console.WriteLine($"Tich: {a * b}");
+        if(b != 0) {
+            Console.WriteLine($"Thuong: {(double)a / b}");
+        } else {
+        Console.WriteLine("Khong the chia cho 0!");
+        }
+        
+        // Bài 3: Kỹ thuật Hoán đổi (Swap)
+        // Yêu cầu: Cho 2 biến x=5, y=10. Hãy đổi giá trị của chúng để x=10, y=5.
+        // Hướng dẫn: Hãy tưởng tượng em có 2 ly nước xanh và đỏ,
+        // để đổi nước cho nhau em cần thêm 1 ly rỗng (biến temp).
+        int x = 5, y = 10;
+        Console.WriteLine($"Trước khi swap: x={x}, y={y}");
+        int temp = x;  // temp = 5
+        x = y;         // x = 10
+        y = temp;      // y = 5
+        Console.WriteLine($"Sau khi swap: x={x}, y={y}");
+            }
+}
